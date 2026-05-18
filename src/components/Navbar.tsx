888265966b0d478710453e347ae0ab1e11e8c5d3
iEbrahem Sai <iebrahemsai944@gmail.com>
2026-05-19 01:46:15 +0400

    update logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -21,14 +21,14 @@ export const Navbar = () => {
           <span className="flex items-center space-x-2 text-2xl font-medium text-indigo-500 dark:text-gray-100">
               <span>
                 <Image
-                  src="/img/logo.svg"
+                  src="/img/uwork.png"
                   width="32"
                   alt="N"
                   height="32"
                   className="w-8"
                 />
               </span>
-            <span>Nextly</span>
+            <span>United Work</span>
           </span>
         </Link>
 
